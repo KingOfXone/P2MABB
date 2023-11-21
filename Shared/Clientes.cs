42c2a6254a5fe5619ac333606f3a45eba8a29d34
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-20 23:44:51 -0400

    Clase Clientes

---
+++ Shared/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2MABB.Shared
+{
+    
+        public class Clientes
+        {
+            [Key]
+            public int ClienteId { get; set; }
+            public required string Nombres { get; set; }
+        }
+    
+}
