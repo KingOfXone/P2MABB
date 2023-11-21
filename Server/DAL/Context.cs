85c1b84ec343e06d9ad3bd64cfede75473749bac
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-20 23:29:18 -0400

    Creando Contexto y guia de radzen

---
+++ Server/DAL/Context.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P2MABB.Server.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
